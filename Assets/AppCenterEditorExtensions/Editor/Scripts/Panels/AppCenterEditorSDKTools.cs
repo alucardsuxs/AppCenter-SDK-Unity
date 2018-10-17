8760ea84df1546c61292acd3071c18f46533b0fd
alex-oreshko <alexander.oreshko@akvelon.com>
2018-10-17 16:22:37 +0300

    Rotating wheel added to "SDK is installing" button

--- Assets/AppCenterEditorExtensions/Editor/Scripts/Panels/AppCenterEditorSDKTools.cs
+++ Assets/AppCenterEditorExtensions/Editor/Scripts/Panels/AppCenterEditorSDKTools.cs
@@ -24,6 +24,7 @@ namespace AppCenterEditor
         private static bool isObjectFieldActive;
         public static bool isSdkSupported = true;
         private static bool isInstalling = false;
+        private static int angle = 0;
 
         public static void DrawSdkPanel()
         {
@@ -170,6 +171,58 @@ namespace AppCenterEditor
             }
         }
 
+        private static Color32[] rotateSquare(Color32[] arr, double phi, Texture2D originTexture)
+        {
+            int x;
+            int y;
+            int i;
+            int j;
+            double sn = Math.Sin(phi);
+            double cs = Math.Cos(phi);
+            Color32[] arr2 = originTexture.GetPixels32();
+            int W = originTexture.width;
+            int H = originTexture.height;
+            int xc = W / 2;
+            int yc = H / 2;
+            for (j = 0; j < H; j++)
+            {
+                for (i = 0; i < W; i++)
+                {
+                    arr2[j * W + i] = new Color32(0, 0, 0, 0);
+                    x = (int)(cs * (i - xc) + sn * (j - yc) + xc);
+                    y = (int)(-sn * (i - xc) + cs * (j - yc) + yc);
+                    if ((x > -1) && (x < W) && (y > -1) && (y < H))
+                    {
+                        arr2[j * W + i] = arr[y * W + x];
+                    }
+                }
+            }
+            return arr2;
+        }
+
+        private static Texture2D RotateImage(Texture2D originTexture, int angle)
+        {
+            Texture2D result;
+            result = new Texture2D(originTexture.width, originTexture.height);
+            Color32[] pix1 = result.GetPixels32();
+            Color32[] pix2 = originTexture.GetPixels32();
+            int W = originTexture.width;
+            int H = originTexture.height;
+            int x = 0;
+            int y = 0;
+            Color32[] pix3 = rotateSquare(pix2, (Math.PI / 180 * (double)angle), originTexture);
+            for (int j = 0; j < H; j++)
+            {
+                for (var i = 0; i < W; i++)
+                {
+                    pix1[result.width / 2 - W / 2 + x + i + result.width * (result.height / 2 - H / 2 + j + y)] = pix3[i + j * W];
+                }
+            }
+            result.SetPixels32(pix1);
+            result.Apply();
+            return result;
+        }
+
         private static void ShowSdkNotInstalledMenu()
         {
             using (new AppCenterGuiFieldHelper.UnityVertical(AppCenterEditorHelper.uiStyle.GetStyle("gpStyleGray1")))
@@ -182,12 +235,13 @@ namespace AppCenterEditor
                 using (new AppCenterGuiFieldHelper.UnityHorizontal(AppCenterEditorHelper.uiStyle.GetStyle("gpStyleGray1")))
                 {
                     var buttonWidth = 200;
-                     
+
                     GUILayout.FlexibleSpace();
                     if (isInstalling)
                     {
                         GUI.enabled = false;
-                        if (GUILayout.Button("SDK is installing", AppCenterEditorHelper.uiStyle.GetStyle("Button"), GUILayout.MaxWidth(buttonWidth), GUILayout.MinHeight(32)))
+                        Texture2D image = RotateImage(AssetDatabase.LoadAssetAtPath("Assets/AppCenterEditorExtensions/Editor/UI/Images/wheel.png", typeof(Texture2D)) as Texture2D, angle++);
+                        if (GUILayout.Button(new GUIContent("  SDK is installing", image), AppCenterEditorHelper.uiStyle.GetStyle("Button"), GUILayout.MaxWidth(buttonWidth), GUILayout.MinHeight(32)))
                         { }
                     }
                     else
@@ -202,7 +256,6 @@ namespace AppCenterEditor
                         }
                     }
                     GUILayout.FlexibleSpace();
-
                 }
             }
         }
